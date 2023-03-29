eeb9b23a59687124d9bdc9feb882f841d6ece81c
mehedi9868 <mehedi9868@gmail.com>
2023-03-29 06:46:00 +0600

    build and deploy to surge:shaggy-book.surge.sh

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
